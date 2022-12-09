5873bb993e0bddb9d46c50b89ef2096427b80408
JiaoXiaoyang12 <105853019+JiaoXiaoyang12@users.noreply.github.com>
2022-12-09 16:36:37 +0800

    Add files via upload

--- createtrigger.sql
+++ createtrigger.sql
@@ -1,6 +1,36 @@
 delimiter $$
-create trigger t_ins_loan after insert on duedate for each row
-	begin
-    update loan set m_duedate=(select count(*)from duedate where new.id=old.id);
-    end $$
+create trigger t_insert_reservation after insert on reservation for each row
+begin
+	update item set m_Reservation=1 where new.id=id and new.librarycode=librarycode;
+    end$$
+delimiter ;
+delimiter $$
+create trigger t_del_reservation after delete on reservation for each row
+begin
+	update item set m_Reservation=0 where id=old.id and librarycode=old.librarycode;
+    end$$
+delimiter ;
+delimiter $$
+create trigger t_insert_losebook after insert on losebook for each row
+begin
+	update item set m_losebook=1 where new.id=id and new.librarycode=librarycode;
+    end$$
+delimiter ;
+delimiter $$
+create trigger t_delete_losebook after delete on losebook for each row
+begin
+	update item set m_losebook=0 where id=old.id and librarycode=old.librarycode;
+    end$$
+delimiter ;
+delimiter $$
+create trigger t_insert_loan after insert on loan for each row
+begin
+	update item set m_Loan=1 where new.id=id and new.librarycode=librarycode;
+    end$$
+delimiter ;
+delimiter $$
+create trigger t_del_loan after delete on loan for each row
+begin
+	update item set m_Loan=0 where id=old.id and librarycode=old.librarycode;
+    end$$
 delimiter ;
